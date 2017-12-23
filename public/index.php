0dfea789f73c267d071ea1eef510c3e2c8177ca8
polidog <polidogs@gmail.com>
2017-12-23 22:33:58 +0900

    change default APP_ENv

--- public/index.php
+++ public/index.php
@@ -10,9 +10,11 @@ require __DIR__.'/../vendor/autoload.php';
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+        $_SERVER['APP_ENV'] = 'prod';
+//        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+    } else {
+        (new Dotenv())->load(__DIR__.'/../.env');
     }
-    (new Dotenv())->load(__DIR__.'/../.env');
 }
 
 if ($_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev'))) {
